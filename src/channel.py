a5bb7859364b518d938073f83201fc2cc43c5251
nikita <n.shavkin@it-park.tech>
2023-10-03 10:03:49 +0300

    fixed

--- src/channel.py
+++ src/channel.py
@@ -10,13 +10,11 @@ API_KEY = os.getenv("API_KEY")
 class Channel:
     """Класс для ютуб-канала"""
 
-    youtube = build('youtube', 'v3', developerKey=API_KEY)
-    channel_id = 'UCwHL6WHUarjGfUM_586me8w'
-    channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.youtube = build('youtube', 'v3', developerKey=API_KEY)
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
