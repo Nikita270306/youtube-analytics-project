127c92eeda0a69b7c63dfef7ea1e235a27b04bd9
nikita <n.shavkin@it-park.tech>
2023-11-01 16:09:45 +0300

    hw6_done

--- src/video.py
+++ src/video.py
@@ -7,80 +7,30 @@ class Video:
 
     def __init__(self, video_id):
         self.video_id = video_id
-        self._video = self.youtube_api.videos().list(id=video_id, part='snippet,contentDetails,'
-                                                                       'statistics,status').execute()
+        self.title = ''
+        self.url = ''
+        self.views = 0
+        self.likes = 0
 
+        self.video_info()
+    def video_info(self) -> None:
+        try:
+            video_info = youtube.videos().list(part='snippet,statistics', id=self.video_id).execute()
+
+            if 'items' in video_info:
+                video_info = video_info['items'][0]
+                self.title = video_info['snippet']['title']
+                self.url = f'https://www.youtube.com/watch?v={self.video_id}'
+                self.views = int(video_info['statistics']['viewCount'])
+                self.likes = int(video_info['statistics']['likeCount'])
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.views = None
+            self.likes = None
+            print('Передан несуществующий id видео')
     def __str__(self):
         return str(self.title)
-
-    def item(self):
-        return self._video.get("items")[0]
-
-    def snippet(self) -> dict | None:
-        return self.item().get('snippet')
-
-    def statistics(self) -> dict | None:
-        return self.item().get('statistics')
-
-    def content_details(self) -> dict | None:
-        return self.item().get('contentDetails')
-
-    @property
-    def id(self):
-        value = self.video_id
-        return value
-
-    @property
-    def title(self):
-        try:
-            value = self.snippet().get('title')
-        except:
-            return None
-        else:
-            return value
-
-    @property
-    def link(self):
-        try:
-            value = f"https://www.youtube.com/watch?v={self.id}"
-        except:
-            return None
-        else:
-            return value
-
-    @property
-    def view_count(self):
-        try:
-            value = self.statistics().get('viewCount')
-        except:
-            return None
-        else:
-            return value
-
-    @property
-    def like_count(self):
-        try:
-            value = self.statistics().get('likeCount')
-        except:
-            return None
-        else:
-            return value
-
-    @property
-    def duration(self):
-        try:
-            value = self.content_details().get('duration')
-        except:
-            return None
-        else:
-            return value
-
-    @staticmethod
-    def printj(dict_to_print: dict) -> None:
-        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-
-    def print_info(self):
-        return self.printj(self._video)
 
 
 class PLVideo(Video):
@@ -88,7 +38,5 @@ class PLVideo(Video):
         super().__init__(video_id)
         self.playlist_id = playlist_id
 
-
     def __str__(self):
         return self.title
-
